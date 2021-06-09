38414ee24bde8d94343b8f7feb6517e2c7b97b58
natalia <nataliarrod@gmail.com>
2021-06-08 19:59:57 -0500

    finished project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from 'react';
+import styled from '@emotion/styled'
+import Phrase from './components/Phrase'
+
+
+const Container = styled.div`
+  display: flex;
+  align-items:center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: green;
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .3s ease;
+
+  :hover{
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+
+  const [phrase, setPhrase] = useState({});
+
+  const searchAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes')
+    const phrase = await api.json()
+    setPhrase(phrase[0]);
+  }
+
+useEffect(() => {
+  searchAPI()
+}, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Phrase 
+        phrase={phrase}
+      />
+      <Button
+        onClick={searchAPI}
+      >
+        Get your phrase
+      </Button>
+    </Container>
+  
   );
 }
 
